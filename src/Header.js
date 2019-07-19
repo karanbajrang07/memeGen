1df5bbfdfe8aad8a91da073911cb9fa7cec9193a
Karan Bajrang Bishnoi <karanbk.bpgc@gmail.com>
2019-07-19 13:12:55 +0530

    Added meme gen files

---
+++ src/Header.js
@@ -0,0 +1,15 @@
+import React from "react"
+
+function Header(){
+  return (
+      <header>
+      <img 
+          src="http://images.nintendolife.com/news/2015/04/the_man_who_created_trollface_wants_a_cut_of_meme_runs_earnings/attachment/0/original.jpg"
+          alt="Problem?"
+          class="center"
+      />
+      </header>
+      
+  )  
+}
+export default Header
